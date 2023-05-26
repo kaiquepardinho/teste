122b1003af825f7afa6854b46f4138fc602b7d5e
Kaique Alves C. Pardinho <kaiquealves2208@gmail.com>
2023-05-26 07:09:29 -0300

    commit 6432432462vgb

---
+++ box77/sobre/script.js
@@ -0,0 +1,205 @@
+function clickMenu3() {
+    if (itens3.style.display == 'block') {
+        itens3.style.display = 'none'
+    } else {
+        itens3.style.display = 'block'
+    }
+}
+
+function clickDown() {
+    if (arrow.style.display == 'block') {
+        arrow.style.display = 'none'
+        pbtnbox.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow.style.display = 'block'
+        pbtnbox.style.display = 'block'
+    }
+}
+
+function clickDown1() {
+    if (arrow1.style.display == 'block') {
+        arrow1.style.display = 'none'
+        pbtnbox1.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow1.style.display = 'block'
+        pbtnbox1.style.display = 'block'
+    }
+}
+
+function clickDown2() {
+    if (arrow2.style.display == 'block') {
+        arrow2.style.display = 'none'
+        pbtnbox2.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow2.style.display = 'block'
+        pbtnbox2.style.display = 'block'
+    }
+}
+
+function clickDown3() {
+    if (arrow3.style.display == 'block') {
+        arrow3.style.display = 'none'
+        pbtnbox3.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow3.style.display = 'block'
+        pbtnbox3.style.display = 'block'
+    }
+}
+
+function clickDown4() {
+    if (arrow4.style.display == 'block') {
+        arrow4.style.display = 'none'
+        pbtnbox4.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow4.style.display = 'block'
+        pbtnbox4.style.display = 'block'
+    }
+}
+
+function clickDown5() {
+    if (arrow5.style.display == 'block') {
+        arrow5.style.display = 'none'
+        pbtnbox5.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow5.style.display = 'block'
+        pbtnbox5.style.display = 'block'
+    }
+}
+
+function clickDown6() {
+    if (arrow6.style.display == 'block') {
+        arrow6.style.display = 'none'
+        pbtnbox6.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow6.style.display = 'block'
+        pbtnbox6.style.display = 'block'
+    }
+}
+
+function clickDown7() {
+    if (arrow7.style.display == 'block') {
+        arrow7.style.display = 'none'
+        pbtnbox7.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow7.style.display = 'block'
+        pbtnbox7.style.display = 'block'
+    }
+}
+
+function clickDown8() {
+    if (arrow8.style.display == 'block') {
+        arrow8.style.display = 'none'
+        pbtnbox8.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow8.style.display = 'block'
+        pbtnbox8.style.display = 'block'
+    }
+}
+
+function clickDown00() {
+    if (arrow00.style.display == 'block') {
+        arrow00.style.display = 'none'
+        pbtnbox00.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow00.style.display = 'block'
+        pbtnbox00.style.display = 'block'
+    }
+}
+
+function clickDown01() {
+    if (arrow01.style.display == 'block') {
+        arrow01.style.display = 'none'
+        pbtnbox01.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow01.style.display = 'block'
+        pbtnbox01.style.display = 'block'
+    }
+}
+
+function clickDown02() {
+    if (arrow02.style.display == 'block') {
+        arrow02.style.display = 'none'
+        pbtnbox02.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow02.style.display = 'block'
+        pbtnbox02.style.display = 'block'
+    }
+}
+
+function clickDown03() {
+    if (arrow03.style.display == 'block') {
+        arrow03.style.display = 'none'
+        pbtnbox03.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow03.style.display = 'block'
+        pbtnbox03.style.display = 'block'
+    }
+}
+
+function clickDown04() {
+    if (arrow04.style.display == 'block') {
+        arrow04.style.display = 'none'
+        pbtnbox04.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow04.style.display = 'block'
+        pbtnbox04.style.display = 'block'
+    }
+}
+
+function clickDown05() {
+    if (arrow05.style.display == 'block') {
+        arrow05.style.display = 'none'
+        pbtnbox05.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow05.style.display = 'block'
+        pbtnbox05.style.display = 'block'
+    }
+}
+
+function clickDown06() {
+    if (arrow06.style.display == 'block') {
+        arrow06.style.display = 'none'
+        pbtnbox06.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow06.style.display = 'block'
+        pbtnbox06.style.display = 'block'
+    }
+}
+
+function clickDown07() {
+    if (arrow07.style.display == 'block') {
+        arrow07.style.display = 'none'
+        pbtnbox07.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow07.style.display = 'block'
+        pbtnbox07.style.display = 'block'
+    }
+}
+
+function clickDown08() {
+    if (arrow08.style.display == 'block') {
+        arrow08.style.display = 'none'
+        pbtnbox08.style.display = 'none'
+        art1.style.background = ''
+    } else {
+        arrow08.style.display = 'block'
+        pbtnbox08.style.display = 'block'
+    }
+}
